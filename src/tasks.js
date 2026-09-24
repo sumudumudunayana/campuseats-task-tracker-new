c6c16f370cb9c20921e293846a484b33c4f2b7c9
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-09-24 12:57:47 +0530

    feat: add initial CampusEats task list

---
+++ src/tasks.js
@@ -0,0 +1,9 @@
+// CampusEats task list
+
+const tasks = [
+  "Design the menu screen",
+  "Build the orders API",
+  "Add user login",
+];
+
+console.log(`CampusEats has ${tasks.length} open tasks`);
